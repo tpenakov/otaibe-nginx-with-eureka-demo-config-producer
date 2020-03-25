d42f9731d73e5c033c915bfe81b5b5c4339d9fb9
Triphon Penakov <triphon@gmail.com>
2020-03-25 14:12:06 +0200

    make docker_run_nginx.sh script to reload docker configuration

--- src/test/resources/shell/docker_run_nginx.sh
+++ src/test/resources/shell/docker_run_nginx.sh
@@ -6,7 +6,15 @@ export WORKDIR=/tmp/$SERVICE_NAME
 mkdir -p $WORKDIR
 curl -o $WORKDIR/default.conf http://localhost:24456/nginx/config
 
-docker run -i --rm -p 24457:24457 --network host \
-          --name=SERVICE_NAME \
+docker run -i -d --rm -p 24457:24457 --network host \
+          --name=$SERVICE_NAME \
           --volume=$WORKDIR:/etc/nginx/conf.d \
           nginx:1.16
+
+x=1
+while [ $x -eq 1 ]
+do
+  sleep 30
+  curl -o $WORKDIR/default.conf http://localhost:24456/nginx/config
+  docker exec -i -d $SERVICE_NAME nginx -s reload
+done
