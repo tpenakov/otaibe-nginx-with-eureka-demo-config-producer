356fb3a4393b671f7e1c91e0d56c61f00a6f4bee
Triphon Penakov <triphon@gmail.com>
2020-03-26 11:47:07 +0200

    added some parameters to docker_run.sh

--- src/test/resources/shell/docker_run.sh
+++ src/test/resources/shell/docker_run.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+echo "=================================================="
+echo "Usage (from project dir):"
+echo "bash src/test/resources/shell/docker_run.sh <EUREKA_HOST_PORT> <HOST_NAME_OR_IP>"
+echo
+echo "Example:"
+echo 'bash src/test/resources/shell/docker_run.sh 172.94.14.97:24455 172.94.14.97'
+echo "=================================================="
+
 set -x #echo on
+
 
 docker run -i -d --rm -p 24456:24456 --network host \
   --name otaibe-nginx-with-eureka-demo-config-producer \
+   --env "EUREKA_CLIENT_SERVICEURL_DEFAULTZONE=http://$1/eureka/" \
+   --env "EUREKA_INSTANCE_HOSTNAME=$2" \
   triphon/otaibe-nginx-with-eureka-demo-config-producer
