5bcc77f40467437c99cb4de158d7d8d9673b75c5
Triphon Penakov <triphon@gmail.com>
2020-03-26 11:33:45 +0200

    start otaibe-nginx-with-eureka-demo-config-producer docker container in detached mode

--- src/test/resources/shell/docker_run.sh
+++ src/test/resources/shell/docker_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x #echo on
 
-docker run -i --rm -p 24456:24456 --network host \
+docker run -i -d --rm -p 24456:24456 --network host \
   --name otaibe-nginx-with-eureka-demo-config-producer \
   triphon/otaibe-nginx-with-eureka-demo-config-producer
